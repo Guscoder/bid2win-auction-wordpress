d0922906314060943b6a793c49ea9442891291c7
Justin <jtwgus@gmail.com>
2016-04-02 17:20:01 -0400

    Updates
    Made text editable in admi

--- page-about-3.php
+++ page-about-3.php
@@ -10,23 +10,20 @@
 
                 <div class="mission-statement">
                    <h1>Mission Statement</h1>
-                   <p>We continually strive to set and improve the standard for providing the services storage facilities need to properly handle delinquent units in a professional, high quality manner with uncompromised integrity.</p>
-                </div>
+                   <p><?php the_field('mission_statement') ?></p>
+                </div> 
 
                 <div class="about">
                     <h1>About BID2WIN LLC</h1>
-                    <p>We are a family owned business with over a decade of specialized experience providing professional services for storage facilities.  Our goal is to make the storage auction process as efficient and stress-free as possible.
-We currently provide services in Chicagoland, Eastern Wisconsin and Northern Indiana.
-Our office is located in Rockford, Michigan.
-                    </p>
+                    <p><?php the_field('about_statement') ?></p>
+
                     <p>Contact Us:</p>
-                    <p>Email: <a href="mailto:bid2winstorageauctions@gmail.com">bid2winstorageauctions@gmail.com</a> </p>
+                    <p>Email: <a href="mailto:<?php the_field('contact_email') ?>"><?php the_field('contact_email') ?></a> </p>
                     <p>616-633-9520</p>
                    
                 </div>
                 
             </section> <!-- #about-container -->
-
 
 
 <?php get_footer(); ?>
